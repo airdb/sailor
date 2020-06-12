6f7170cdc55bba0d7ebf500d340e4fc0f042adc7
Dean Zhang <dean@airdb.com>
2020-06-13 03:06:20 +0800

    update

--- dbutils/dbutils.go
+++ dbutils/dbutils.go
@@ -3,84 +3,53 @@ package dbutils
 import (
 	"fmt"
 	"log"
+	"os"
 	"sync"
 	"sync/atomic"
 
 	_ "github.com/go-sql-driver/mysql" // golint
 	"github.com/jinzhu/gorm"
-	"github.com/spf13/viper"
 )
 
 var dbs sync.Map
 var hasInit int32
 var hasPend sync.Mutex
 
-const (
-	DBHOST   = "DB_HOST"
-	DBPORT   = "DB_PORT"
-	DBUSER   = "DB_USER"
-	DBPASSWD = "DB_PASSWD"
-	DBNAME   = "DB_NAME"
-)
+func InitDB(dbNames []string) {
+	for _, dbName := range dbNames {
+		if atomic.LoadInt32(&hasInit) == 1 {
+			return
+		}
 
-const DefaultEnvPrefix = ""
+		hasPend.Lock()
+		defer hasPend.Unlock()
 
-func InitDefault() {
-	InitDB(DefaultEnvPrefix)
-}
+		if atomic.LoadInt32(&hasInit) == 1 {
+			log.Println("cocurrent_between_coroutines")
+			return
+		}
 
-func InitDB(dbname string) {
-	if atomic.LoadInt32(&hasInit) == 1 {
-		return
+		gdbc := os.Getenv(dbName)
+
+		conn, err := gorm.Open("mysql", gdbc)
+		if err != nil {
+			log.Println("Error: connect to db server failed, ", gdbc, err)
+			panic("Error: connect to db server failed")
+		} else {
+			log.Println("Connect to db success")
+		}
+
+		conn.LogMode(true)
+		conn.SingularTable(true)
+
+		gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
+			return defaultTableName + "_tab"
+		}
+
+		dbs.Store(dbName, conn)
+
+		atomic.StoreInt32(&hasInit, 1)
 	}
-
-	hasPend.Lock()
-	defer hasPend.Unlock()
-
-	if atomic.LoadInt32(&hasInit) == 1 {
-		log.Println("cocurrent_between_coroutines")
-		return
-	}
-
-	viper.New()
-
-	if dbname != DefaultEnvPrefix {
-		viper.SetEnvPrefix(dbname)
-	}
-
-	viper.AutomaticEnv()
-
-	gdbc := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		viper.GetString(DBUSER),
-		viper.GetString(DBPASSWD),
-		viper.GetString(DBHOST),
-		viper.GetString(DBPORT),
-		viper.GetString(DBNAME),
-	)
-
-	db, err := gorm.Open(
-		"mysql",
-		gdbc,
-	)
-
-	if err != nil {
-		log.Println("Error: connect to db server failed, ", gdbc, err)
-		panic("Error: connect to db server failed")
-	} else {
-		log.Println("Connect to db success")
-	}
-
-	db.LogMode(true)
-	db.SingularTable(true)
-
-	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
-		return defaultTableName + "_tab"
-	}
-
-	dbs.Store(viper.GetString(DBNAME), db)
-
-	atomic.StoreInt32(&hasInit, 1)
 }
 
 func WriteDB(name string) *gorm.DB {
@@ -91,17 +60,7 @@ func ReadDB(name string) *gorm.DB {
 	return DB(name)
 }
 
-func DefaultDB() (db *gorm.DB) {
-	return DB(DefaultEnvPrefix)
-}
-
 func DB(name string) (db *gorm.DB) {
-	InitDB(name)
-
-	if name == DefaultEnvPrefix {
-		name = viper.GetString(DBNAME)
-	}
-
 	_db, ok := dbs.Load(name)
 	if ok {
 		db = _db.(*gorm.DB)
