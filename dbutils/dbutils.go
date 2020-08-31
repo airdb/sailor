1b7faa205f5d7d983b522af9a0dca07040b0bf5b
deancn <dean@airdb.com>
2020-08-31 23:42:57 +0800

    update

--- dbutils/dbutils.go
+++ dbutils/dbutils.go
@@ -1,14 +1,17 @@
 package dbutils
 
 import (
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 	"log"
 	"os"
 	"strings"
 	"sync"
 	"sync/atomic"
+	"time"
 
-	_ "github.com/go-sql-driver/mysql" // golint
-	"github.com/jinzhu/gorm"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var dbs sync.Map
@@ -24,26 +27,42 @@ func InitDB(dbNames []string) {
 	defer hasPend.Unlock()
 
 	for _, dbName := range dbNames {
-		gdbc := os.Getenv(dbName)
+		dsn := os.Getenv(dbName)
 
-		if !strings.Contains(gdbc, "?") {
-			gdbc += "?charset=utf8&parseTime=True"
+		if !strings.Contains(dsn, "?") {
+			dsn += "?charset=utf8&parseTime=True"
 		}
 
-		conn, err := gorm.Open("mysql", gdbc)
+		newLogger := logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+			logger.Config{
+				SlowThreshold: time.Second, // Slow SQL threshold
+				LogLevel:      logger.Info, // Log level
+				Colorful:      false,       // Disable color
+			},
+		)
+
+		conn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+			Logger: newLogger,
+			NamingStrategy: schema.NamingStrategy{
+				// TablePrefix:   "t_", // table name prefix, table for `User` would be `t_users`
+				SingularTable: true, // use singular table name, table for `User` would be `user` with this option enabled
+			},
+		})
+
 		if err != nil {
-			log.Printf("Error: connect to databases %s failed, err is %v\n", gdbc, err)
+			log.Printf("Error: connect to databases %s failed, err is %v\n", dsn, err)
 			panic("Error: connect to db server failed")
 		} else {
 			log.Printf("Connect to %s successfully.\n", dbName)
 		}
 
-		conn.LogMode(true)
-		conn.SingularTable(true)
+		// conn.LogMode(true)
+		// conn.SingularTable(true)
 
-		gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
-			return defaultTableName + "_tab"
-		}
+		// gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
+		// 	return defaultTableName + "_tab"
+		// }
 
 		dbs.Store(dbName, conn)
 	}
