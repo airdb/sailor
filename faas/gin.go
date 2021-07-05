4ff1ec401430ce3bf6070197f12ebee254569ed4
deancn <dean@airdb.com>
2021-07-05 09:07:07 +0800

    support chi

---
+++ faas/gin.go
@@ -0,0 +1,35 @@
+package faas
+
+import (
+	"context"
+
+	"github.com/airdb/sailor/deployutil"
+	"github.com/gin-gonic/gin"
+	"github.com/serverless-plus/tencent-serverless-go/events"
+	"github.com/serverless-plus/tencent-serverless-go/faas"
+
+	ginAdapter "github.com/serverless-plus/tencent-serverless-go/gin"
+)
+
+var GinFaas *ginAdapter.GinFaas
+
+// Handler serverless faas handler.
+func Handler(ctx context.Context, req events.APIGatewayRequest) (events.APIGatewayResponse, error) {
+	return GinFaas.ProxyWithContext(ctx, req)
+}
+
+func RunTencentGin(r *gin.Engine) {
+	if deployutil.GetDeployStage() == deployutil.DeployStageDev {
+		defaultAddr := ":8081"
+		err := r.Run(defaultAddr)
+		if err != nil {
+			panic(err)
+		}
+
+		return
+	}
+
+	GinFaas = ginAdapter.New(r)
+
+	faas.Start(Handler)
+}
--- go.mod
+++ go.mod
@@ -3,16 +3,15 @@ module github.com/airdb/sailor
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.6.3
+	github.com/gin-gonic/gin v1.7.2
+	github.com/go-chi/chi/v5 v5.0.3
 	github.com/google/go-querystring v1.0.0
-	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/miekg/dns v1.1.29
-	github.com/pkg/errors v0.8.1
+	github.com/pkg/errors v0.9.1
+	github.com/serverless-plus/tencent-serverless-go v1.0.1
 	github.com/smartystreets/goconvey v1.6.4
 	github.com/swaggo/gin-swagger v1.2.0
-	golang.org/x/net v0.0.0-20200625001655-4c5254603344
-	golang.org/x/tools v0.0.0-20200731060945-b5fad4ed8dd6 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974
 	gorm.io/driver/mysql v1.0.1
 	gorm.io/gorm v1.20.0
 )
