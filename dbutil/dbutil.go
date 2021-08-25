13d0752bb8c657f45d23dbad2d7be678a8713f89
deancn <dean@airdb.com>
2021-08-25 09:56:45 +0800

    add sql log

--- dbutil/dbutil.go
+++ dbutil/dbutil.go
@@ -100,18 +100,20 @@ func Connect(dbName string) (db *gorm.DB) {
 		dsn += "?charset=utf8mb4&parseTime=True&loc=Local"
 	}
 
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-		logger.Config{
-			SlowThreshold: time.Second, // Slow SQL threshold
-			// LogLevel:      logger.Info, // Log level
-			LogLevel: logger.Silent, // Log level
-			Colorful: false,         // Disable color
-		},
-	)
+	/*
+		newLogger := logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+			logger.Config{
+				SlowThreshold: time.Second, // Slow SQL threshold
+				// LogLevel:      logger.Info, // Log level
+				LogLevel: logger.Silent, // Log level
+				Colorful: false,         // Disable color
+			},
+		)
+	*/
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: newLogger,
+		Logger: logger.Default.LogMode(logger.Info),
 		NamingStrategy: schema.NamingStrategy{
 			TablePrefix:   "tab_", // table name prefix, table for `User` would be `t_users`
 			SingularTable: true,   // use singular table name, table for `User` would be `user` with this option enabled
