20bdedd4d0b1c1a6f0990b781b7e482244f6ca95
deancn <dean@airdb.com>
2022-03-04 22:07:14 +0800

    update

--- asn2ip/asn2ip_test.go
+++ asn2ip/asn2ip_test.go
@@ -1,6 +1,7 @@
 package asn2ip_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/airdb/sailor/asn2ip"
@@ -10,7 +11,10 @@ func TestRun(t *testing.T) {
 	t.Log("TestRun")
 
 	fetcher := asn2ip.NewFetcher("whois.radb.net", 43)
-	ips, err := fetcher.Fetch(true, false, "15169")
+
+	asnum := "AS15169"
+	asnum = strings.Replace(asnum, "AS", "", -1)
+	ips, err := fetcher.Fetch(true, false, asnum)
 	if err != nil {
 		t.Error(err)
 		return
